4ea1787ae108b8e7c94a2287910470c84f482546
Javier Chavez <javcho@skrin.tech>
2023-04-10 22:28:24 -0500

    Created logic to validate token

--- src/auth/validate.js
+++ src/auth/validate.js
@@ -1,5 +1,7 @@
 module.exports = function (request, response) {
   console.log(request)
   const headers = request.headers 
-  return response.end(JSON.stringify(headers))
+  const token = headers.authorization
+  if(!token) return response.json(403, {message:"Manda token oe"})
+  return response.json({message: "Bienvenido"})
 }
